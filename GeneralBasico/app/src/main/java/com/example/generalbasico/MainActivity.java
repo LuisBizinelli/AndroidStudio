901f6ffd4ab8b12499cf2ed68a90320615fef348
Luis Henrique Bizinelli <luis_bizinelli@hotmail.com>
2019-08-22 20:26:42 -0300

    #2
    Construção inicial

--- GeneralBasico/app/src/main/java/com/example/generalbasico/MainActivity.java
+++ GeneralBasico/app/src/main/java/com/example/generalbasico/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.generalbasico;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView Vencedor, Dados;
+    private Button Jogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Vencedor = findViewById(R.id.Vencedor);
+        Dados = findViewById(R.id.Dados);
+
+        Jogar = findViewById(R.id.Jogar);
+
     }
 }
